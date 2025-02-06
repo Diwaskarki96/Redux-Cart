342d0829737f7a802785702f6da06de90ef33599
Diwash <diwaskarki96@gmail.com>
2025-02-06 17:26:00 +0545

    minor changes

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -28,7 +28,7 @@ const CartContainer = () => {
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>{total ? `${total.toFixed(2)}` : 0.0}</span>
+            total <span>${total ? `${total.toFixed(2)}` : 0.0}</span>
           </h4>
         </div>
         <button
