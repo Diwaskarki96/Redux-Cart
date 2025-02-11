9c166f280e8f96228774d430c5eda7785d236a7e
Diwash <diwaskarki96@gmail.com>
2025-02-11 15:57:51 +0545

    minor changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import { closeModal } from "../store/slice/modalSlice";
 import { clearCart } from "../store/slice/cartSlice";
